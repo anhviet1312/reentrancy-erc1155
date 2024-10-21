5f98290f57c022f2d8d20a39c663755f117695a8
viethq <quocviet13122002@gmail.com>
2024-10-21 17:56:57 +0700

    update

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,33 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract NFT is ERC721 {
-    uint public tokenId;
-    uint public maxSupply = 1000;
- 
-    uint public constant DEFAULT_PRICE = 1 gwei;
-    mapping(address => bool) public addressMinted;
-    constructor() ERC721("VulnerableNFT", "VNFT") {}
 
-    function mintNFT() external payable {
-        require(DEFAULT_PRICE <= msg.value, "Insufficient payable value");
-        require(tokenId < maxSupply, "All tokens have been minted");
-        require(!addressMinted[msg.sender], "Address has minted");
 
-        unchecked {
-            ++tokenId;
-        }
-        _safeMint(msg.sender, tokenId);
-        
-        addressMinted[msg.sender] = true;
+
+contract VulnerableVault is ERC1155 {
+
+    struct LockAsset {
+        uint256 fnftId;
+        uint256 depositAmount;
+    }
+   // mapping(uint256 => uint256) public depositAmount;
+    uint256 public fnftId;
+    constructor() ERC1155("") {}
+
+
+
+    // Mint new FNFT
+    function mintFNFT(uint256 quantity, uint256 depositAmount) external payable{
+        uint256 totalDeposit = depositAmount * quantity;
+        require(totalDeposit == msg.value, "not equal amount");
+        _mint(msg.sender, ++fnftId, quantity, "");
     }
 
-    function getTokenId() public view returns (uint) {
-        return tokenId;
+
+    function depositAdditionalToFNFT(uint256 quantity, uint256 depositAmount) external {
+        safeTransferFrom(msg.sender, address(this), fnftId, 1, "");
     }
+
+    // function withdraw() external {
+    //     uint256 amount = depositAmount[fnftId];
+    //     depositAmount[fnftId] = 0;
+    //     payable(msg.sender).transfer(amount);
+    // }
+
+    receive() external payable {}
 }
